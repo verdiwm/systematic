eb04e44b1bc3ff4ab9df47f32ac40cbfa63d1902
Federico Maria Morrone <contact@morrone.dev>
2025-02-02 18:24:28 +0100

    Add cstr helper

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,27 @@
 pub mod sys;
 
 pub mod login;
+
+use std::{
+    borrow::Cow,
+    ffi::{CStr, CString},
+    io,
+};
+
+pub trait CStrArg {
+    fn into_c_str(&self) -> io::Result<Cow<'_, CStr>>;
+}
+
+impl CStrArg for &str {
+    fn into_c_str(&self) -> io::Result<Cow<'_, CStr>> {
+        Ok(Cow::Owned(
+            CString::new(*self).map_err(|_| io::Error::from_raw_os_error(22))?,
+        ))
+    }
+}
+
+impl CStrArg for &CStr {
+    fn into_c_str(&self) -> io::Result<Cow<'_, CStr>> {
+        Ok(Cow::Borrowed(&self))
+    }
+}
--- src/login.rs
+++ src/login.rs
@@ -1,9 +1,9 @@
-use std::{ffi::CStr, io};
+use std::io;
 
-use crate::sys;
+use crate::{sys, CStrArg};
 
-pub fn session_is_active(session: &CStr) -> io::Result<bool> {
-    let ret = unsafe { sys::sd_session_is_active(session.as_ptr()) };
+pub fn session_is_active<C: CStrArg>(session: C) -> io::Result<bool> {
+    let ret = unsafe { sys::sd_session_is_active(session.into_c_str()?.as_ptr()) };
 
     if ret < 0 {
         return Err(io::Error::from_raw_os_error(ret.wrapping_neg()));
