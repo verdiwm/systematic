3bd16821ae185eaaee6fa6dc871262e5c9b975c5
Matilde Morrone <contact@morrone.dev>
2025-02-02 17:20:35 +0100

    Reduce minimum systemd version to 235

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     pkg_config::Config::new()
-        .atleast_version("255")
+        .atleast_version("235")
         .probe("libsystemd")
         .expect("Failed to link to libsystemd");
 }
